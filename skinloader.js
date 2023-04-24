7897901f40496e1c523680fa1963a68521c0e7bb
Alexandr Baranovskyy <alex.bar.dev@gmail.com>
2023-04-24 19:04:41 +0300

    new fetch link

--- skinloader.js
+++ skinloader.js
@@ -1,12 +1,25 @@
+
+
+// db.collection("skins").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data()}`);
+//     });
+// });
+//main
 let content = document.querySelector("#content");
 getSkinData();
 
 async function getSkinData() {
-    const res = await fetch('http://localhost:8080/skins');
-    const data = await res.json();
-    for(const skin of data) {
-        content.appendChild(await loadSkins(skin));
+    try {
+        const res = await fetch('https://anubisdunk-serverloader.onrender.com/skins');
+        const data = await res.json();
+        for (const skin of data) {
+            content.appendChild(await loadSkins(skin));
+        }
+    } catch (e) {
+        console.log("404")
     }
+
 
 }
 
@@ -20,3 +33,5 @@ async function loadSkins(skins) {
     div.innerHTML = card;
     return div;
 }
+
+
